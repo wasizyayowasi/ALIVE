bb6194678cce0d3d19af56b181e10d37c03c2d83
わしじゃよわし <114457086+wasizyayowasi@users.noreply.github.com>
2024-03-17 00:05:34 +0900

    更新

--- util/Easing.h
+++ util/Easing.h
@@ -8,6 +8,15 @@
 /// https://qiita.com/HnniTns/items/9e3799d3b414dccbbd7d
 /// </summary>
 
+namespace
+{
+	//’l
+	constexpr float float_value_1 = 1.0f;
+
+	//’l
+	constexpr float float_value_2 = 2.0f;
+}
+
 namespace Easing {
 
 	template <typename T>
@@ -50,34 +59,38 @@ namespace Easing {
 
 namespace Easing
 {
-	template <typename T>
-	T InOutCubic(T elapsedTime, T totalTime, T maxValue, T currentValue) {
+	template <typename T = float>
+	T InOutCubic(T elapsedTime, T totalTime, T maxValue, T currentValue) 
+	{
 		maxValue -= currentValue;
 		elapsedTime /= totalTime;
 
-		if (elapsedTime / 2.0f < 1.0f) {
-			return maxValue / 2.0f * elapsedTime * elapsedTime * elapsedTime + currentValue;
+		if (elapsedTime / float_value_2 < float_value_1)
+		{
+			return maxValue / float_value_2 * elapsedTime * elapsedTime * elapsedTime + currentValue;
 		}
 
 		elapsedTime -= maxValue;
 
-		return maxValue / 2.0f * (elapsedTime * elapsedTime * elapsedTime + 2.0f) + currentValue;
+		return maxValue / float_value_2 * (elapsedTime * elapsedTime * elapsedTime + float_value_2) + currentValue;
 	}
 
-	template <typename T>
-	T OutCirc(T elapsedTime, T totalTime, T maxValue, T currentValue) {
+	template <typename T = float>
+	T OutCirc(T elapsedTime, T totalTime, T maxValue, T currentValue)
+	{
 		maxValue -= currentValue;
-		elapsedTime /= (totalTime - 1.0f);
+		elapsedTime /= (totalTime - float_value_1);
 
-		elapsedTime = (std::max)((std::min)(elapsedTime, 1.0f), -1.0f);
+		elapsedTime = (std::max)((std::min)(elapsedTime, float_value_1), -float_value_1);
 
-		return maxValue * std::sqrt(1.0f - elapsedTime * elapsedTime) + currentValue;
+		return maxValue * std::sqrt(float_value_1 - elapsedTime * elapsedTime) + currentValue;
 	}
 
-	template <typename T>
-	T InOutSine(T elapsedTime, T totalTime, T maxValue, T currentValue) {
+	template <typename T = float>
+	T InOutSine(T elapsedTime, T totalTime, T maxValue, T currentValue)
+	{
 		maxValue -= currentValue;
 
-		return -maxValue / 2 * (std::cos(elapsedTime * DX_PI_F / totalTime) - 1) + currentValue;
+		return -maxValue / float_value_2 * (std::cos(elapsedTime * DX_PI_F / totalTime) - float_value_1) + currentValue;
 	}
 }
